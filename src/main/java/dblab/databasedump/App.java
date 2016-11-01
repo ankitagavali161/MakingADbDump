a6c9171ea713a9021b3dd9626b1d2c49911231fb
ankitagavali <ankitaofficial161@gmail.com>
2016-11-01 21:13:24 +0100

    Creating a DB Dump

---
+++ src/main/java/dblab/databasedump/App.java
@@ -0,0 +1,14 @@
+package dblab.databasedump;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//db2sql db2 = new db2sql("config.properties");
+        System.out.println( "Hello World!" );
+    }
+}
